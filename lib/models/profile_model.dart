7349e07ff987aa3634abe130a13621ea591f2ce2
william-saputra <williammatthews.id@gmial.com>
2024-12-06 02:16:03 +0700

    update model for profile

--- lib/models/profile_model.dart
+++ lib/models/profile_model.dart
@@ -3,38 +3,41 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class Profile {
-  String username;
-  String? profilePicture;
-  String description;
-  List<Restaurant> favoriteRestaurants;
-  List<Food> favoriteFoods;
+  final String username;
+  final String? profilePicture;
+  final String description;
+  final List<Restaurant> favoriteRestaurants;
+  final List<Food> favoriteFoods;
 
   Profile({
     required this.username,
-    this.profilePicture,
+    required this.profilePicture,
     required this.description,
     required this.favoriteRestaurants,
     required this.favoriteFoods,
   });
 
   factory Profile.fromJson(Map<String, dynamic> json) {
-    var restaurantsJson = json['favorite_restaurants'] as List;
-    var foodsJson = json['favorite_foods'] as List;
+    var restoList = json['favorite_restaurants'] as List<dynamic>;
+    List<Restaurant> favoriteRestaurants = restoList.map((resto) => Restaurant.fromJson(resto)).toList();
+
+    var foodList = json['favorite_foods'] as List<dynamic>;
+    List<Food> favoriteFoods = foodList.map((food) => Food.fromJson(food)).toList();
 
     return Profile(
       username: json['username'],
       profilePicture: json['profile_picture'],
       description: json['description'],
-      favoriteRestaurants: restaurantsJson.map((i) => Restaurant.fromJson(i)).toList(),
-      favoriteFoods: foodsJson.map((i) => Food.fromJson(i)).toList(),
+      favoriteRestaurants: favoriteRestaurants,
+      favoriteFoods: favoriteFoods,
     );
   }
 }
 
 class Restaurant {
-  int id;
-  String name;
-  String location;
+  final int id;
+  final String name;
+  final String location;
 
   Restaurant({required this.id, required this.name, required this.location});
 
@@ -45,13 +48,16 @@ class Restaurant {
       location: json['location'],
     );
   }
+
+  @override
+  String toString() => name;
 }
 
 class Food {
-  int id;
-  String name;
-  String category;
-  int price;
+  final int id;
+  final String name;
+  final String category;
+  final double price;
 
   Food({required this.id, required this.name, required this.category, required this.price});
 
@@ -60,18 +66,43 @@ class Food {
       id: json['id'],
       name: json['name'],
       category: json['category'],
-      price: json['price'],
+      price: (json['price'] as num).toDouble(),
     );
   }
+
+  @override
+  String toString() => name;
 }
 
-// Fungsi untuk mengambil data profile dari API
-Future<Profile?> fetchProfileData() async {
-  final response = await http.get(Uri.parse('http://localhost:8000/user/get_client_data/'));
+// Fetching profile data using API
+// Future<Profile?> fetchProfileData() async {
+//   final response = await http.get(Uri.parse('http://localhost:8000/user/get_client_data/'));
 
-  if (response.statusCode == 200) {
-    return Profile.fromJson(json.decode(response.body)['data']);
-  } else {
-    throw Exception('Failed to load profile data');
-  }
-}
+//   if (response.statusCode == 200) {
+//     return Profile.fromJson(json.decode(response.body)['data']);
+//   } else {
+//     throw Exception('Failed to load profile data');
+//   }
+// }
+
+// // Update username
+// Future<bool> updateUsername(String newName) async {
+//   final response = await http.post(
+//     Uri.parse('http://localhost:8000/user/profile/change-name/'),
+//     headers: {'Content-Type': 'application/json'},
+//     body: json.encode({'new_value': newName}),
+//   );
+
+//   return response.statusCode == 200;
+// }
+
+// // Update description
+// Future<bool> updateDescription(String newDescription) async {
+//   final response = await http.post(
+//     Uri.parse('http://localhost:8000/user/profile/change-description/'),
+//     headers: {'Content-Type': 'application/json'},
+//     body: json.encode({'new_value': newDescription}),
+//   );
+
+//   return response.statusCode == 200;
+// }
