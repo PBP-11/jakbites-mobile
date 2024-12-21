b8b6407d02406823e374fe8c63a7ee6257c075be
alphasuthamedia <emalphasuthamedia@gmail.com>
2024-12-21 12:48:44 -0500

    resto done

--- lib/restaurant/restaurant_detail.dart
+++ lib/restaurant/restaurant_detail.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:jakbites_mobile/models/resutarant_model.dart';
+import 'package:jakbites_mobile/widgets/left_drawer.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -62,16 +63,33 @@ class _RestaurantPageDetailState extends State<RestaurantPageDetail> {
     return listReviewRestaurant;
   }
 
+  Future<List<Food>> fetchFood(CookieRequest request) async {
+    final response =
+        await request.get('http://localhost:8000/json_food/');
+
+    // Melakukan decode response menjadi bentuk json
+    var data = response;
+
+    // Melakukan konversi data json menjadi object Food
+    List<Food> listFood = [];
+    for (var d in data) {
+      if (d != null) {
+        // print();
+        // print(d['restaurant'] == widget.resto.pk);
+        if (d['fields']['restaurant'] == widget.resto.pk) {
+          listFood.add(Food.fromJson(d));
+        }
+      }
+    }
+    return listFood;
+  }
+
   @override
   Widget build(BuildContext context) {
     final request = context.watch<CookieRequest>();
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.all(10),
-        child: Align(
-        child: Column(
-        children: [
-          FutureBuilder(
+      appBar: AppBar(
+        title: FutureBuilder(
             future: fetchReviewRestaurant(request), 
             builder: (context, AsyncSnapshot snapshot) {
               return Builder(
@@ -101,6 +119,13 @@ class _RestaurantPageDetailState extends State<RestaurantPageDetail> {
               );
             }
           ),
+      ),
+      // drawer: const LeftDrawer(),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: Align(
+        child: Column(
+        children: [
           SizedBox(
             height: 15,
           ),
@@ -280,9 +305,86 @@ class _RestaurantPageDetailState extends State<RestaurantPageDetail> {
                   ),
                 ))
               ],
-            ): Container(),
+            ): Container(
+              child: FutureBuilder(
+                future: fetchFood(request),
+                builder: (context, AsyncSnapshot snapshot) {
+                  if (snapshot.data == null) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else {
+                    if (!snapshot.hasData) {
+                      return const Column(
+                        children: [
+                          Text(
+                            'Belum ada data makanan pada JakBites',
+                            style: TextStyle(fontSize: 20, color: Color(0xff59A5D8)),
+                          ),
+                          SizedBox(height: 8),
+                        ],
+                      );
+                    } else {
+                      return ListView.builder(
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: (_, index) => Container(
+                          padding: const EdgeInsets.all(2),
+                          child: Column(
+                            children: [
+                              Card(
+                                shape: const RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(8.0))),
+                                child: InkWell(
+                                  onTap: () => Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          FoodPageDetail(snapshot.data![index]),
+                                    ),
+                                  ),
+                                  child: Column(
+                                    children: <Widget>[
+                                      ListTile(
+                                        title: Text(
+                                          "${snapshot.data![index].fields.name}",
+                                          style: const TextStyle(
+                                            fontSize: 18.0,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        subtitle: Text(
+                                          "${snapshot.data![index].fields.category}",
+                                          style: const TextStyle(
+                                            fontSize: 12.0,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        trailing: const Icon(Icons.restaurant),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              // const Image(
+                              // image: AssetImage('lib/assets/images/resto.png'),
+                              // ),
+                              // Text(
+                              //   "${snapshot.data![index].fields.name}",
+                              //   style: const TextStyle(
+                              //     fontSize: 18.0,
+                              //     fontWeight: FontWeight.bold,
+                              //   ),
+                              // ),
+                            ],
+                          ),
+                        ),
+                      );
+                    }
+                  }
+                },
+              ),
+            ),
           ),
-        ],
+        ],  
       ),
       ),
       ), 
